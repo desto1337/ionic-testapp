6ec1306a671ca9bc7cc9f537a929de0ed39e8fa9
Dennis Stoklosa <dennis.stoklosa@sparqs.io>
2019-05-16 14:33:02 +0200

    created first example app, which will catch data from rest service via angular methods

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  url = 'http://www.omdbapi.com/';
+  apiKey = '49233cd7'; // Insert your generated API Key here
+
+  constructor(private http: HttpClient) { }
+
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`)
+    .pipe(
+      map(results => {
+        console.log('RAW: ', results);
+        // tslint:disable-next-line:no-string-literal
+        return results['Search'];
+      })
+    );
+  }
+
+  getDetails(id: string) {
+    return this.http.get(`${this.url}?i=${encodeURI(id)}&plot=full&apikey=${this.apiKey}`)
+  }
+}
